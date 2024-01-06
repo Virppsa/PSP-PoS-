29d746882081b4c6eca6fdee2f685b3cf1dadca6
Virppsa <greta.virpsaite@gmail.com>
2024-01-06 18:27:13 +0200

    Added user migration for the DB.

---
+++ PspPos/Migrations/20240102205218_User.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PspPos.Migrations
+{
+    /// <inheritdoc />
+    public partial class User : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "User_Id",
+                table: "Users",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Users",
+                newName: "User_Id");
+        }
+    }
+}
