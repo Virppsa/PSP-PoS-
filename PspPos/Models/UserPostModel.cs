3cab52e370dafc9697c2e01d7f00e851bdf69744
Vilius <vilius.bakutis23@gmail.com>
2024-01-07 14:38:37 +0200

    intial implementation

--- PspPos/Models/UserPostModel.cs
+++ PspPos/Models/UserPostModel.cs
@@ -5,12 +5,12 @@ namespace PspPos.Models
 {
     public class UserPostModel
     {
-        public Guid CompanyId { get; set; }
-        public int StoreId { get; set; }
+        public Guid? StoreId { get; set; }
         public string? Role { get; set; }
         public string? Name { get; set; }
         public string? Email { get; set; }
         public string? Phone { get; set; }
         public string? Address { get; set; }
+        public int? LoyaltyPoints { get; set; }
     }
 }
