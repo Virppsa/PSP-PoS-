ac27488917b9bc5f986df62225ce47c5acac2121
Virppsa <greta.virpsaite@gmail.com>
2024-01-07 15:14:45 +0200

    Add stores model and migration

---
+++ PspPos/Models/Store.cs
@@ -0,0 +1,20 @@
+﻿using PspPos.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PspPos.Models
+{
+    public class Store
+    {
+        [Key]
+
+        public Guid Guid { get; set; }
+        [ForeignKey("Company")]
+        public Guid CompanyId { get; set; }
+        public string? Address { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+
+        public virtual Company Company { get; set; }
+    }
+}
